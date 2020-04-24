ab23c78e5241dd0369c2ffbda3120f4de73f7652
mai cook <mailiecook@yahoo.com>
2020-04-24 14:43:10 -0700

    added select, and option tags in index, added to if statement in getElements

--- src/main.js
+++ src/main.js
@@ -2,24 +2,40 @@
 import { Currency } from './../src/currency.js';
 import $ from 'jquery';
 import './style.css';
+import Swal from 'sweetalert2';
 
 $(document).ready(function () {
 $("#montey").submit(function (event){
   event.preventDefault();
   let input = parseInt($("input").val());
+  let type = $("#type").val();
   console.log(input);
   (async () => {
-    let currency = new Currency();
-    const response = await currency.getExchangeInfo(input);
-    getElements(response);
+    let currency = new Currency(input);
+    const response = await currency.getExchangeInfo();
+    getElements(response,type);
   })();
-  function getElements(response) {
-    
+
+  function getElements(response, type) {
   
-  if (response) {
+    if (!response && type === null) {
+     
+      Swal.fire("whoops sorry currency doesn't exist");
+  
+  }else if (response && type === "AED") {
     console.log(response);
-    $("#output").text(`${response.conversion_rates.AED}`*input);
+    $("#output").text(`${type}`+":"+`${response.conversion_rates.AED}`*input);
+  } else if (response && type === "EUR") {
+     $("#output").text(`${type}`+":"+`${response.conversion_rates.EUR}`*input);
+  } else if (response && type === "KRW") {
+    $("#output").text(`${type}` + ":" + `${response.conversion_rates.KRW}` * input);
+    
+  } else if (response && type === "CNY") {
+     $("#output").text(`${type}` + ":" + `${response.conversion_rates.KRW}` * input);
+    } else {
+      swal.fire("Error Error ERROR ERROR");
     }
   }
+
   });
 });
