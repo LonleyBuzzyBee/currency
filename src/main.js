3245a653e50ba143e273c18e7844f9a991eb4162
mai cook <mailiecook@yahoo.com>
2020-04-24 12:58:54 -0700

    had to restart project folder/repo api key was shoing in commit history :(

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// import swal from 'sweetalert2';
+import { Currency } from './../src/currency.js';
+import $ from 'jquery';
+import './style.css';
+
+$(document).ready(function () {
+$("#montey").submit(function (event){
+  event.preventDefault();
+  let input = $("input").val();
+  console.log(input);
+  (async () => {
+    let currency = new Currency();
+    const response = await currency.getExchangeInfo(input);
+    getElements(response);
+  })();
+  function getElements(response) {
+    
+  
+  if (response) {
+    console.log(response);
+    swal.fire("yooooo");
+    }
+  }
+  });
+});
